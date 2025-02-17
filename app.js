d09b03a79f17f3fb1e28125c0161ed1e8e625007
Santiago Ferreira <santiandresfer@hotmail.com>
2025-02-17 18:48:39 -0300

    Array, función y validación del campo de entrada para añadir amigos

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. 
+// Aquí deberás desarrollar la lógica para resolver el problema.
+
+//Primer paso del desafío. Crear un array para almacenar los nombres de amigos
+let arrayAmigos = []
+
+//Capturando el valor del campo de entrada del usuario
+let input = document.getElementById('amigo')
+
+let listaAmigosUl = document.getElementById('listaAmigos')
+
+//Validación del campo de entrada y actualización del arrayAmigos
+function agregarAmigo() {
+    if (!input.value) {
+        alert("Porfavor, inserte un nombre")
+    } else {
+        arrayAmigos.push(input.value)
+        listaAmigosUl.innerHTML += `<li>${input.value}</li>` 
+    }
+    limpiarInput()
+}
+
+//Función para limpiar el campo de entrada una vez añadido los nombres
+function limpiarInput() {
+    document.querySelector('#amigo').value = '';
+}
+
